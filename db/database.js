d2c1b5742b135c4c5d10ae42781d9a0518925a73
bhumesh789 <bhumeshkewat10@gmail.com>
2022-08-24 17:49:22 +0530

    crud with knex

---
+++ db/database.js
@@ -0,0 +1,22 @@
+const knex = require('knex')({
+    client:'mysql',
+    connection:{
+        user:'root',
+        host:'localhost',
+        database:'crud1',
+        password:'1'
+    }
+})
+knex.schema.createTable('crud',(table)=>{
+    table.increments('id').primary()
+    table.string('name').notNullable()
+    table.string('email').unique().notNullable()
+    table.string('password')
+    table.integer('age')
+})
+.then(()=>{
+    // console.log('table created successfullly......');
+}).catch(()=>{
+    console.log('table allready exist.....');
+})
+module.exports = knex
---
+++ listen.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const app = express();
+const knex = require('./db/database');
+const port = 3000;
+
+app.use(express.json());    
+
+//  insert data
+app.post('/post',async(req,res)=>{
+    try {
+        const data = await knex('crud').insert(req.body)
+        res.send(req.body)
+    } catch (error) {
+        res.send('data not insert successfully...')
+    }
+})
+
+// show all user data
+app.get('/show',async(req,res)=>{
+    try {
+        const info = await knex('crud')
+        res.send(info)
+    } catch (error) {
+        res.send('something error')
+    }
+})
+
+// show single user data
+
+app.get('/single/:id',async(req,res)=>{
+    try {
+        const log = await knex('crud').where({id:req.params.id})
+        if(log != 0){
+            // console.log('data not');
+            res.send(log)
+        }
+        else{
+            res.send('user data not found')
+        }
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+// user login
+
+app.get('/login',async(req,res)=>{
+    try {
+        const log = await knex('crud').where(req.body)
+        if(log != 0){
+            // console.log('data not');
+            res.send(log)
+        }
+        else{
+            res.send('user data not found')
+        }
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+// update user data
+
+app.put('/update/:id',async(req,res)=>{
+    try {
+        const log = await knex('crud').where({id:req.params.id}).update(req.body)
+        if(log != 0){
+            res.send({message:'user data update successfully',status:req.body})
+        }
+        else{
+            res.send('data not updated')
+        }
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+// delete user data
+
+app.delete('/delete/:id',async(req,res)=>{
+    try {
+        const log = await knex('crud').where({id:req.params.id}).delete(req.body)
+        if(log != 0){
+            res.send('data deleted successfully...')
+        }
+        else{
+            res.send("user not found")
+        }
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+
+app.listen(port,()=>{
+    console.log(`server running at ${port}`);
+})
